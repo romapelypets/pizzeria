ef10c6a8814eb49f8a6c6cb7c056442ef3b4bf49
romapelypets <pelypets.ri@gmail.com>
2016-12-11 14:57:00 +0200

    Add PizzaController

---
+++ Pizzeria.WebUI/Controllers/PizzaController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Pizzeria.DAL.Models;
+using Pizzeria.DAL.Repository;
+
+namespace Pizzeria.WebUI.Controllers
+{
+    public class PizzaController : Controller
+    {
+        private IUnitOfWork unitOfWork;
+        // GET: Pizza
+        public PizzaController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+        public ActionResult Index()
+        {
+            return View(unitOfWork.PizzaRepository.GetAll().ToList());
+        }
+       
+        public ActionResult Delete(int id)
+        {
+            unitOfWork.PizzaRepository.Delete(id);
+            unitOfWork.Save();
+            return RedirectToAction("Index");
+        }
+    }
+}
